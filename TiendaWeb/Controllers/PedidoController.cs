2d11dbf4112a6ab3fe502494f5d5ff3e83e86a3a
acalvom <andreac.3108@gmail.com>
2021-02-17 13:02:36 +0100

    Añadir controlador y vista para mostrar los Pedidos de un usuario

---
+++ TiendaWeb/Controllers/PedidoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendaWeb.Models;
+
+namespace TiendaWeb.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private tiendaWeb db = new tiendaWeb();
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View(db.Pedidos.ToList());
+        }
+    }
+}
